a6b89eeaf7d8f9664f114b8cb5b0f09081ebecd3
sherwyndd <sherwynd2007@gmail.com>
2024-07-04 00:55:44 +0700

    Create Missing Number.cpp

---
+++ Additional Problems/Missing Number.cpp
@@ -0,0 +1,39 @@
+/*
+    Harder not smarter
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define IOS ios::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define F first
+#define S second
+#define nl "\n"
+typedef pair<int,int> p;
+const int N = 1'000'007;
+const int N2 = 3'000;
+const int INF = 1'000'000'000'000'000'000;
+const int base = 311;
+const int MOD = 1'000'000'007;
+const int MULTITEST = 0;
+void enter(){
+    int n;
+    cin >> n;
+    int Sum = 0;
+    for (int i = 1; i < n; ++i){
+        int x;
+        cin >> x;
+        Sum += x;
+    }
+    cout << n * (n + 1) / 2 - Sum;
+}
+void solve(){
+}
+signed main(){
+    //freopen("main.inp","r",stdin);
+    //freopen("main.out","w",stdout);
+    IOS;
+    int q = 1;
+    if (MULTITEST) cin >> q;
+    while (q--){enter(); solve();}
+}
