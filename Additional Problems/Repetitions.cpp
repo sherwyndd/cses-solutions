f406438b2c20913c7697b00f5bf26ea14546bddd
sherwyndd <sherwynd2007@gmail.com>
2024-07-04 00:56:38 +0700

    Create Repetitions.cpp

---
+++ Additional Problems/Repetitions.cpp
@@ -0,0 +1,44 @@
+/*
+    Harder not smarter
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define IOS ios::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define F first
+#define S second
+#define nl "\n"
+typedef pair<int,int> p;
+const int N = 1'000'007;
+const int N2 = 3'000;
+const int INF = 1'000'000'000'000'000'000;
+const int base = 311;
+const int MOD = 1'000'000'007;
+const int MULTITEST = 0;
+string s;
+void enter(){
+    cin >> s;
+    int t = 1;
+    int res = 1;
+    for (int i = 1; i < s.size(); ++i){
+        if (s[i] == s[i - 1]){
+            t++;
+            res = max(res, t);
+        }
+        else{
+            t = 1;
+        }
+    }
+    cout << res;
+}
+void solve(){
+}
+signed main(){
+    //freopen("main.inp","r",stdin);
+    //freopen("main.out","w",stdout);
+    IOS;
+    int q = 1;
+    if (MULTITEST) cin >> q;
+    while (q--){enter(); solve();}
+}
